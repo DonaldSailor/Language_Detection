a5c17d2afd965aaad23d7f612e112fa0a1285b16
KacperWol <250087@student.pwr.edu.pl>
2023-10-25 00:00:30 +0200

    Update

--- server/language_detector.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-class LanguageDetector:
-    def __init__(self, model):
-        self.detector = pipeline(model)
-
-
-    def detect(self, texts_list):
-        results = self.detector(texts_list)
